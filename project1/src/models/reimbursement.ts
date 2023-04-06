454c338a1d66981bbf5abac5c33d4c1a55ecdc0d
Joshua <jpb1492@gmail.com>
2023-04-06 15:15:44 -0400

    ReimbursementList

--- project1/src/models/reimbursement.ts
+++ project1/src/models/reimbursement.ts
@@ -2,12 +2,16 @@
 
 export class Reimbursement {
     id: number;
+    user_id: number;
     description: string;
     amount: number;
+    status: number; // Status: 1,2,3 => Approved, Pending, Denied
     
-    constructor(id: number, description: string, amount: number){
+    constructor(id: number, user_id: number, description: string, amount: number, status: number) {
         this.id = id;
+        this.user_id = user_id;
         this.description = description;
         this.amount = amount;
+        this.status = status;
     }
 }

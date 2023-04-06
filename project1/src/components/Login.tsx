fd080aa8c332e87124ca76eb3e295ddab071e5fe
Arie <127146558+brodrigo1111@users.noreply.github.com>
2023-04-06 12:00:10 -0400

    dashboard working

--- project1/src/components/Login.tsx
+++ project1/src/components/Login.tsx
@@ -1,11 +1,18 @@
 import { SyntheticEvent, useState } from "react";
 import { User } from "../models/user";
+import { Navigate } from 'react-router-dom'
 
-export default function Login(){
+interface ILoginProps{
+    currentUser: User | undefined;
+    setCurrentUser: (newUser: User) => void;
+}
+
+export default function Login(props: ILoginProps){
 
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const [principal, setPrincipal] = useState<User>();
+    const [errorMessage, setErrorMessage] = useState('');
+    const [redirect, setRedirect] = useState(false);
 
     let updateUsername = (e: SyntheticEvent) => {
         setUsername((e.target as HTMLInputElement).value);
@@ -14,6 +21,7 @@ export default function Login(){
         setPassword((e.target as HTMLInputElement).value);
     }
     let submitLogin = async (e: SyntheticEvent) => {
+        setErrorMessage('');
         if (username && password && username.length > 3 && username.length < 17 && password.length > 7 && password.length < 21) {
             console.log("success");
             try {
@@ -25,31 +33,31 @@ export default function Login(){
                     body: JSON.stringify({username, password})
                 });
                 if (response.status === 201) {
-                    setPrincipal(await response.json());
-                    console.log(principal);
+                    props.setCurrentUser(await response.json());
+                    console.log(props.currentUser);
+                    setRedirect(true);
                 } else {
-                    alert('Username and/or password incorrect. Please try again.');
+                    setErrorMessage('Username and/or password incorrect. Please try again.');
                 }
             } catch (err) {
                 console.log(err);
             }
         } else {
-            alert("Usernames must be between 4 and 16 characters. Passwords must be between 8 and 20 characters.")
+            setErrorMessage("Usernames must be between 4 and 16 characters. Passwords must be between 8 and 20 characters.")
         }
     }
 
-    return (
-        principal ?
-        <>
-            Hello! Your user_id: {principal.user_id}
-        </>
+    return (     
+        props.currentUser && redirect ?
+        <Navigate to='/' />
         :
-        <>        
+        <>
         <p>Login</p>
         <div>
             <input type="text" id="login-username" placeholder="Username" onChange={updateUsername}/>
             <input type="text" id="login-password" placeholder="Password" onChange={updatePassword}/>
             <button id="login-button" onClick={submitLogin}>Submit</button>
+            <p>{errorMessage}</p>
         </div>
         </>
     );
